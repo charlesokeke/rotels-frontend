f701eb07e920c70234c71a8cafcb208b7974fef1
charles <okekeemeka20@gmail.com>
2019-06-15 21:57:52 -0400

    Updates

---
+++ precache-manifest.d3bce9fd67b7834277b117ead69cf5bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb99da58adbbceb56595",
+    "url": "/rotels-frontend/static/js/runtime~main.c03f0c83.js"
+  },
+  {
+    "revision": "37bd79a9ac91bf655cab",
+    "url": "/rotels-frontend/static/js/main.a124f08d.chunk.js"
+  },
+  {
+    "revision": "1ad24cc4b8476e386186",
+    "url": "/rotels-frontend/static/js/2.3f334f61.chunk.js"
+  },
+  {
+    "revision": "37bd79a9ac91bf655cab",
+    "url": "/rotels-frontend/static/css/main.dda758ea.chunk.css"
+  },
+  {
+    "revision": "9b0ecbecdd7c916499817bb28ee8c81b",
+    "url": "/rotels-frontend/index.html"
+  }
+];
