dbebc72e731bdfbf022532618bdde853a81dae82
charles <okekeemeka20@gmail.com>
2019-10-07 18:24:41 -0400

    Updates

--- precache-manifest.b21b32aa0427ec314e28f44c71157f7c.js
+++ precache-manifest.b21b32aa0427ec314e28f44c71157f7c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rotels-frontend/static/js/runtime~main.c03f0c83.js"
   },
   {
-    "revision": "2ec88a3ad46e7f8d6595",
-    "url": "/rotels-frontend/static/js/main.a298ee90.chunk.js"
+    "revision": "cb1f716de1860c9d4a2b",
+    "url": "/rotels-frontend/static/js/main.32f50314.chunk.js"
   },
   {
     "revision": "a6f8a3b83eaaeb3bcf0d",
     "url": "/rotels-frontend/static/js/2.242c228b.chunk.js"
   },
   {
-    "revision": "2ec88a3ad46e7f8d6595",
-    "url": "/rotels-frontend/static/css/main.93a83891.chunk.css"
+    "revision": "cb1f716de1860c9d4a2b",
+    "url": "/rotels-frontend/static/css/main.17ebe37f.chunk.css"
   },
   {
-    "revision": "8458ad319a72c5f3b257b19c32303cac",
+    "revision": "a997ee02a358e2918fcad90c45337770",
     "url": "/rotels-frontend/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rotels-frontend/precache-manifest.b0b621201fc410d8135c496016a6aedd.js"
+  "/rotels-frontend/precache-manifest.b21b32aa0427ec314e28f44c71157f7c.js"
 );
 
 workbox.clientsClaim();
